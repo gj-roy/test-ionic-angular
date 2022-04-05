085c14876759920eade5081064f5559ab2cbf8b3
loitp <freuss47@gmail.com>
2022-04-05 17:56:05 +0700

    update

--- src/app/component/component.page.ts
+++ src/app/component/component.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-component',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  goComponent() {
+    this.router.navigate(['component']);
+  }
 }
