5733be4ea5c4f8c8ef4424427e9cbb20713e0a5b
loitp <freuss47@gmail.com>
2022-04-05 11:53:27 +0700

    update

--- src/app/animals/animals.page.ts
+++ src/app/animals/animals.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-animals',
@@ -8,12 +9,16 @@ import { Router } from '@angular/router';
 })
 export class AnimalsPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private navController: NavController) { }
 
   ngOnInit() {
   }
 
-  goBack() {
+  goToFolderInbox() {
     this.router.navigate(['folder/Inbox']);
+  }
+
+  goBack() {
+    this.navController.back();
   }
 }
